1b15f19f3eeeac4aded1dfcd590d3355c2949065
Carlos84sh <Cs.huelves@gmail.com>
2020-10-01 17:13:14 +0000

    finalizar ejercicio

--- src/main.js
+++ src/main.js
@@ -1,5 +1,18 @@
 require('./style.css')
 
-window.load = function() {
-    console.log('Hello world')
+window.onload = function() {
+    
+    let pronombres =['mi','el', 'tu'];
+    let adjetivos =['redonda','conocido','marginado'];
+    let nombres=['casa','pepito', 'juan'];
+
+    let i,m,n;
+
+    for(i=0;i<pronombres.length;i++){
+        for(m=0;m<adjetivos.length;m++){
+            for(n=0;n<nombres.length;n++){
+               console.log(pronombres[i]+adjetivos[m]+nombres[n]+'.com');
+            }
+        }
+    }
 }
